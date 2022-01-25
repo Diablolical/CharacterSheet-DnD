63a7cec07eaac9414cb85ff3ea892440005af61c
Austin Wilson <59572688+austinwilson1@users.noreply.github.com>
2022-01-25 14:53:43 -0600

    Fix node server

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 'use strict'
 
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.set('port', (process.env.PORT) || 5000);
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
+
+/*Init*/
+app.listen(app.get('port'), () => console.log("Server initiated and listening on port: " + app.get('port')));
