d9549a94b796500aefafd00cdf1c5b5813ac3328
Austin Wilson <59572688+austinwilson1@users.noreply.github.com>
2022-01-25 16:54:24 -0600

    Add plus sign to positive modifiers

--- client/src/components/Stats/Attributes/Attribute.js
+++ client/src/components/Stats/Attributes/Attribute.js
@@ -5,10 +5,11 @@ function Attribute ({ attribute, defaultScore }) {
     console.log("Attribute " + attribute + " with default score " + defaultScore)
     const [score, setScore] = useState(defaultScore)
     console.log("Using score: " + score);
+    let mod = calcMod(score);
     return (
         <div class="attributeContainer">
             <label class="attrLabel">{attribute.charAt(0).toUpperCase() + attribute.slice(1)}</label>
-            <span class="attrMod">{calcMod(score)}</span>
+            <span class="attrMod">{(mod > 0) ? "+" + mod : mod}</span>
             <div class="attrScore">
                 <input type="text" name="{attribute}" class="attrInput" value={score} onChange={event => setScore(event.target.value)}/>
             </div>
