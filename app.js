97ba625bae59bd3bdab4b5ff3c20b586ed92ecff
Austin Wilson <59572688+austinwilson1@users.noreply.github.com>
2022-01-25 13:20:45 -0600

    Restructure and configure heroku deploy

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const express = require('express')
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
