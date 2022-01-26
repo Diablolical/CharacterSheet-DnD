a080a57d828c2423877041f59e511bc88f6b25ca
Austin Wilson <59572688+austinwilson1@users.noreply.github.com>
2022-01-25 18:54:58 -0600

    Formatting. . .

--- client/src/components/Shared/NumericInput.js
+++ client/src/components/Shared/NumericInput.js
@@ -1,7 +1,7 @@
 const NumericInput = props => 
-<div class="formInput smallInput">
-    <input type="number" class="numericInput" placeholder=" " {...props}/>
-    <label>{props.name.charAt(0).toUpperCase() + props.name.slice(1)}</label>
-</div>
+    <div class="formInput smallInput">
+        <input type="number" class="numericInput" placeholder=" " {...props}/>
+        <label>{props.name.charAt(0).toUpperCase() + props.name.slice(1)}</label>
+    </div>
 
 export default NumericInput
