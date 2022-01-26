134c67ef12cce82fd90c50e270acefe97969ed90
Austin Wilson <59572688+austinwilson1@users.noreply.github.com>
2022-01-25 18:49:17 -0600

    Remove logs

--- client/src/components/Stats/Attributes/Attribute.js
+++ client/src/components/Stats/Attributes/Attribute.js
@@ -2,9 +2,7 @@ import { useState } from 'react'
 import { calcMod } from '../../Shared/helpers'
 
 function Attribute ({ attribute, defaultScore }) {
-    console.log("Attribute " + attribute + " with default score " + defaultScore)
     const [score, setScore] = useState(defaultScore)
-    console.log("Using score: " + score);
     let mod = calcMod(score);
     return (
         <div class="attributeContainer">
