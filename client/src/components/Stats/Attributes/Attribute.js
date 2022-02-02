ef827c68b6e98053ea252edfeb091d4428d0cb71
Austin Wilson <59572688+austinwilson1@users.noreply.github.com>
2022-02-02 16:23:24 -0600

    Only update ability scores on blur or enter key

--- client/src/components/Stats/Attributes/Attribute.js
+++ client/src/components/Stats/Attributes/Attribute.js
@@ -1,13 +1,37 @@
+import { useState } from 'react';
 import { calcMod } from '../../Shared/helpers'
 
 function Attribute ({ attribute, updateAttributeScore }) {
+
+    const [score, updateScore] = useState(attribute.score)
+
+    const handleEnter = (e) => {
+        if (e.keyCode === 13) {
+            handleBlur()
+        }
+    };
+    
+    const handleBlur = () => {
+        updateAttributeScore(attribute.name, score)
+    };
+
     let mod = calcMod(attribute.score);
     return (
         <div className="attributeContainer">
             <label className="attrLabel">{attribute.name.charAt(0).toUpperCase() + attribute.name.slice(1)}</label>
             <span className="attrMod">{(mod > 0) ? "+" + mod : mod}</span>
             <div className="attrScore">
-                <input type="number" min="1" max="30" name="{attribute.name}" className="attrInput" value={attribute.score} onChange={(e) => updateAttributeScore(attribute.name, e.target.value)} />
+                <input 
+                    type="number"
+                    min="1"
+                    max="30"
+                    name="{attribute.name}"
+                    className="attrInput"
+                    value={score}
+                    onChange={(e) => updateScore(e.target.value)}
+                    onBlur={handleBlur}
+                    onKeyUp={handleEnter}
+                />
             </div>
         </div>
     )
